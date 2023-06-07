bb3c7da7bd7e278e692a83fdbbd61d8ed29ffa3d
timothy <effiongtimothy1@gmail.com>
2023-06-07 20:34:50 +0100

    first commit

--- chat_app/models.py
+++ chat_app/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Permission, Group
 from django.db import models
 
 
@@ -7,9 +7,12 @@ from django.db import models
 
 class User(AbstractUser):
     profile_picture = models.CharField(max_length=250, blank=True, null=True)
-    last_seen = models.DateTimeField(auto_now_add=True)
-    request = models.ForeignKey('Request', on_delete=models.CASCADE)
-    notification = models.ForeignKey('Notification', on_delete=models.CASCADE)
+    last_seen = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    request = models.ForeignKey('Request', on_delete=models.CASCADE, null=True, blank=True)
+    notification = models.ForeignKey('Notification', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.get_full_name()
 
 
 class Request(models.Model):
--- chat_app_project/settings.py
+++ chat_app_project/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-mt4-w$p@6tnw#zfo=r00i1lc*f4q0fo+keoq+ait6ugv$d8@u(
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'chat_app_project.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -79,14 +76,12 @@ WSGI_APPLICATION = 'chat_app_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'chat_app',
+        'USER': ' ',
+        'PASSWORD': '',
         'HOST': 'localhost',
-        'PORT': 3306,
-        'OPTIONS': {
-            'read_default_file': os.path.join(BASE_DIR, 'database_credentials.txt'),
-        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -106,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -118,7 +112,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
@@ -128,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'chat_app.User'
